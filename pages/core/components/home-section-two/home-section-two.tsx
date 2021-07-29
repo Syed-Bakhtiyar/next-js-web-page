03381b46024c35b7305b4b291dc3e79c677edffd
muhammad.bakhtiyar <muhammad.bakhtiyar@tenpearls.com>
2021-07-29 11:23:10 +0500

    carousel has been implemented

--- pages/core/components/home-section-two/home-section-two.tsx
+++ pages/core/components/home-section-two/home-section-two.tsx
@@ -1,109 +1,85 @@
 import { FormControl, TextField, Select, InputLabel, MenuItem, Button } from '@material-ui/core';
 import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import styles from '../../../../styles/Home-section-one.module.css';
-import bgImage from '../../../../public/assets/bg-image.webp';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { useState } from 'react';
+import styles from '../../../../styles/Home-section-two.module.css';
+import finanzCheck from '../../../../public/assets/FFG-Finanzcheck-T.png';
+import goldIcon from '../../../../public/assets/ekomi_gold_siegel_finanzcheck_de.png';
+import topZinsIcon from '../../../../public/assets/top-zins-garantie-seal.png';
 
+let index = 0;
 export default function HomeSectionTwo() {
   const laufZeits = [1,2,3,4,5,6,7,8,9,10];
   const verwendungszwecks = ['Umschuldung', 'Gebrauchtfahrzeug', 'Neufahrzeug', 'Einrichtung/Möbel', 'Renovierung', 'Urlaub', 'PC/TV/Hifi/Video', 'Ausgleich Girokonto', 'Umzug', 'Immobilienfinanzierung', 'Freie Verwendung'];
+  const carouselItems = [
+    'Die Beratung war sehr freundlich. Leider war die Änderung der Telefonnum...',
+    'Ich war und bin mit der Beratung vom Herrn Rein sehr zufrieden. So stel...',
+    'Schnelle kompetente Beratung, nicht aufdringlich, sondern sachlich und z...',
+    'Ich war und bin mit der Beratung vom Herrn Rein sehr zufrieden. So stel...',
+    'Schnelle kompetente Beratung, nicht aufdringlich, sondern sachlich und z...',
+    'sehr angenehmer Kontakt',
+    'Sehr gute, schnelle Abwicklung, tolle Beratung. Alles Gute bis nächste...',
+    'Sehr kompetent, schnelle und unproblematisch Abwicklung. Sehr zu empfeh...',
+    'Sehr kompetent, hilfreich und menschlich. Sehr empfehlenswert',
+    'Angebot der Tagobank, sehr empfehlenswert. Schnelle Abwicklung, freundli...',
+    'Alles ok',
+    'Sehr sehr sehr zufrieden schnell und zuverlässig herzlichen Dank'
+  ];
+  const [displayedCarouselItem, setDisplayCarouselItem] = useState(carouselItems.slice(0, 3));
+
+  
+
+  const nextPrevious = (action: String) => {
+    if(action === 'next'){
+      if(index === (carouselItems.length - 1) || carouselItems[carouselItems.length - 1] === displayedCarouselItem[displayedCarouselItem.length - 1]){
+        return;
+      }
+      index++;
+      setDisplayCarouselItem(carouselItems.slice(index, (index + 3)));
+      return;
+    }
+    if(index === 0 || carouselItems[0] === displayedCarouselItem[0]){
+      return;
+    }
+    index--;
+    setDisplayCarouselItem(carouselItems.slice(index, (index + 3)));
+  }
+
   return (
-    <div className={styles.mainFirstSectionContainer}>
-          <img className={styles.bgImageStyling} src={bgImage.src}/>
-          <div className={`row ${styles.mainGridStyling}`}>
-            <div className="col-md-6 col-sm-12">
-              <div className={styles.mainInfoContainer}>
-                <p className={styles.mainContentInfoStyle}>
-                  Die Kreditexperten
-                </p>
-                <p className={`${styles.mainContentInfoStyle} ${styles.mainContentInfoStyleContent}`}>
-                  Online vergleichen, persönlich beraten
-                </p>
-
-                <div className={styles.mainFormContainer}>
-                    <FormControl className={`${styles.mainFormInputFields} ${styles.mainFormInputFieldsContainer}`} >
-                      <TextField className={styles.mainFormInputFields} id="standard-basic" label="Wunschbetrag" />
-                    </FormControl>
-                    
-                    <FormControl className={`${styles.mainFormInputFields} ${styles.mainFormInputFieldsContainer}`}>
-                      <InputLabel id="demo-simple-select-label">Laufzeit</InputLabel>
-                      <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                      >
-                        {
-                          laufZeits.map(laufzeit => <MenuItem value={laufzeit}>{laufzeit}&nbsp; Jahre</MenuItem>)
-                        }
-                      </Select>
-                    </FormControl>
-
-                    <FormControl className={`${styles.mainFormInputFields} ${styles.mainFormInputFieldsContainer}`}>
-                      <InputLabel id="demo-simple-select-label">Verwendungszweck</InputLabel>
-                      <Select
-                        labelId="demo-simple-select-label"
-                        id="demo-simple-select"
-                      >
-                        {
-                          verwendungszwecks.map(verwendungszweck => <MenuItem value={verwendungszweck}>{verwendungszweck}</MenuItem>)
-                        }
-                      </Select>
-                    </FormControl>
-                    <div className={`${styles.mainFormInputFieldsContainer} ${styles.formWarningContainer}`}>
-                        <div className={styles.formWarningHeading}>
-                          Günstigster Kredit aus über 60 Angeboten ab:
-                        </div>
-                        <div className={`${styles.formWarningHeading} ${styles.formWarningContent}`}>
-                          Günstigster Kredit aus über 60 Angeboten ab:
-                        </div>
-                    </div>
-                    <Button className={`${styles.mainFormInputFields} ${styles.mainFormInputFieldsContainer} ${styles.mainFormSubmitBtnStyling}`}>
-                      <div style={{display: 'flex'}}>
-                        <span>
-                          Kreditvergleich starten
-                        </span>
-                        <ArrowForwardIcon className={styles.arrowSubmitBtn}/>
-                      </div>
-                      <div className={styles.btnContent}>
-                        (Kostenlos und unverbindlich)
-                      </div>
-                    </Button>
-                    {/* <TextField className={styles.mainFormInputFields} id="standard-basic" label="Wunschbetrag" />
-                    <TextField className={styles.mainFormInputFields} id="standard-basic" label="Wunschbetrag" /> */}
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6 col-sm-12">
-              <div className={styles.homeCircleContainer}>
-                <div className={styles.homeCircle}>
-                    <div>
-                      Schon ab
-                    </div>
-                    <h1>
-                      -0,4%
-                    </h1>
-                    <div>
-                      effektiver
-                    </div>
-                    <div>
-                      Jahreszins¹
-                    </div>
-                </div>
-                <div className={styles.listContainer}>
-                  <div className={styles.flexBox}>
-                    <CheckCircleIcon className={styles.checkIcon}/> &nbsp; &nbsp; <span>100% kostenlos</span>
-                  </div>
-                  <div className={styles.flexBox}>
-                    <CheckCircleIcon className={styles.checkIcon}/> &nbsp; &nbsp; <span>Unverbindliche Kreditanfrage</span>
-                  </div>
-                  <div className={styles.flexBox}>
-                    <CheckCircleIcon className={styles.checkIcon}/> &nbsp; &nbsp; <span>99,3% positive Bewertungen</span>
-                  </div>
-                </div>
-              </div>
-              
-            </div>
+    <div>
+      <div className={`row ${styles.sectionTwoGridStyling}`}>
+        <div className={`${styles.flexBoxDisplay} col-md-6 col-sm-12`} >
+          <div>
+            <img src={finanzCheck.src}/>
           </div>
-          {/* <Image  src={bgImage} objectFit="cover"/> */}
+          <div className={styles.sectionTwoIconsContainerStyling}>
+            <img src={goldIcon.src}/>
+          </div>
+          <div className={styles.sectionTwoIconsContainerStyling}>
+            <img src={topZinsIcon.src}/>
+          </div>
         </div>
+        <div className="col-md-6 col-sm-12">
+          <p className={styles.sectionTwoParaStyle}>
+            <a href="https://www.finanzcheck.de/lp/minuszins/"><u>¹-0,4% Effektiver Jahreszins</u> </a> bei: 1.000€ Netto-Darlehensbetrag, 12 Monate Laufzeit, 0,00% fester Sollzins p.a., monatliche Rate 83,15€, Gesamtbetrag 997,83€, Fidor Bank | Entspricht zugleich dem repräsentativen Beispiel gem. § 6a PAngV. Kredit erfordert Online-Kontoauszug (Kontoblick) und Eröffnung eines Girokontos bei der Fidor Bank. Das Girokonto ist während der Kreditlaufzeit kostenlos.
+          </p>
+        </div>
+      </div>
+      <div className={`${styles.flexBoxDisplay} ${styles.sectionTwoHeadng}`}>
+        <p>Das sagen unsere Kunden</p>
+      </div>
+      <div className={`${styles.flexBoxDisplay} ${styles.centerAlignment}`}>
+        <ChevronLeftIcon onClick={()=>{nextPrevious('prev')}}/>
+        {
+          displayedCarouselItem.map((item, index)=>{
+            return  <p key={index} className={styles.sectionTwoCarouselItemToDisplay}>
+                      {item}
+                    </p>
+          })
+        }
+        <ChevronRightIcon onClick={()=>{nextPrevious('next')}}/>
+      </div>
+    </div>
   )
 }
